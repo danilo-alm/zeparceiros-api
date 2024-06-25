7429fae0e5cead62adec247879ac540710b8b929
Danilo Almeida <cs.daniloalmeida@gmail.com>
2024-06-25 16:14:42 -0300

    feat: initial commit

---
+++ src/main/java/com/danilo/zeparceiros/ZeParceirosApplication.java
@@ -0,0 +1,13 @@
+package com.danilo.zeparceiros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeParceirosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeParceirosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/danilo/zeparceiros/ZeParceirosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilo.zeparceiros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeParceirosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
